f77083211a5d2109d83f8df20e34779f33297aa0
michal <michal.bieganowski@cyberuslabs.com>
2019-12-19 12:42:48 +0100

    geoserver integration additional info

--- project/apps/identity_provider/urls_openid.py
+++ project/apps/identity_provider/urls_openid.py
@@ -1,8 +1,10 @@
 from django.urls import path, include
 
 from .views.oidc_stateless import StatelessAuthorizeView
+from .views.oidc_stateless import GeoserverTokenIntrospectionView
 
 urlpatterns = [
     path('', include('oidc_provider.urls', namespace='oidc_provider')),
     path('authorize/stateless/', StatelessAuthorizeView.as_view()),
+    path('introspect/geoserver/', GeoserverTokenIntrospectionView.as_view()),
 ]
--- project/apps/identity_provider/views/oidc_stateless.py
+++ project/apps/identity_provider/views/oidc_stateless.py
@@ -1,6 +1,9 @@
 import re
 from oidc_provider.views import AuthorizeView
 
+from oidc_provider.views import TokenIntrospectionView
+from oidc_provider.lib.endpoints.introspection import TokenIntrospectionEndpoint
+
 
 class StatelessAuthorizeView(AuthorizeView):
     """
@@ -20,3 +23,29 @@ class StatelessAuthorizeView(AuthorizeView):
                 response._headers['location'] = ('Location', response._headers['location'][1].replace('&state=', ''))
 
         return response
+
+
+class GeoserverTokenIntrospectionView(TokenIntrospectionView):
+    """
+    Token introspection providing access_token info shaped according to Geonode for Geoserver integration.
+
+    Usage of this endpoint should be as limited as possible.
+    """
+
+    def post(self, request, *args, **kwargs):
+        introspection = TokenIntrospectionEndpoint(request)
+
+        try:
+            introspection.validate_params()
+            dic = {
+                'client_id': introspection.params['client_id'],
+                'username': introspection.id_token['nickname'],
+                'issued_to': introspection.id_token['nickname'],
+                'email': introspection.id_token['email'],
+                'access_token': introspection.token.access_token,
+                'access_type': 'online',
+                'active': True,
+            }
+            return TokenIntrospectionEndpoint.response(dic)
+        except Exception:
+            return TokenIntrospectionEndpoint.response({'active': False})
