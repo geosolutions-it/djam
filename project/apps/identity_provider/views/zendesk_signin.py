0750a171808d24679375f904b79b4daefc0990f3
Piotr Dankowski <44587161+petrus7@users.noreply.github.com>
2020-01-16 13:19:12 +0100

    add zendesk login view and its settings (#41)
    add zendesk login view and its settings

---
+++ project/apps/identity_provider/views/zendesk_signin.py
@@ -0,0 +1,29 @@
+import time
+import jwt
+import uuid
+
+from django.http import HttpResponseRedirect
+from django.views.generic import View
+from django.conf import settings
+
+
+class ZendeskSigninRedirect(View):
+
+    def get(self, request, *args, **kwargs):
+        redirect_location = self._zendesk_redirect(request)
+        return HttpResponseRedirect(redirect_location)
+
+    def _zendesk_redirect(self, request):
+        payload = {
+            'iat': int(time.time()),
+            'jti': str(uuid.uuid1()),
+            'name': request.user.get_full_name(),
+            'email': request.user.email
+        }
+
+        subdomain = settings.ZENDESK_SUBDOMAIN
+        shared_key = settings.ZENDESK_SECRET
+
+        jwt_value = jwt.encode(payload, shared_key)
+        location = f'https://{subdomain}.zendesk.com/access/jwt?jwt={jwt_value}'
+        return location
--- project/urls.py
+++ project/urls.py
@@ -2,7 +2,6 @@ from django.contrib import admin
 from django.urls import path, re_path, include
 from django.conf import settings
 
-
 urlpatterns = [
     path(r'admin/', admin.site.urls),
     path('', include('apps.user_management.urls')),
