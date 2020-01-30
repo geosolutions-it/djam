4427f6d4623b3ce99c3768fa61c58a8a86b66da8
bieganowski <57660086+bieganowski@users.noreply.github.com>
2020-01-30 09:22:22 +0100

    disable login for users without confirmed email (#75)

--- project/apps/user_management/forms.py
+++ project/apps/user_management/forms.py
@@ -4,10 +4,12 @@ from django.core.exceptions import ObjectDoesNotExist
 from django.contrib.sites.shortcuts import get_current_site
 from django.contrib.auth import get_user_model
 from django.contrib.auth.tokens import default_token_generator
-from django.contrib.auth.forms import UserCreationForm, UserChangeForm, PasswordResetForm
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm, PasswordResetForm, AuthenticationForm
 from django.forms import ModelForm
 from django.utils.encoding import force_bytes
 from django.utils.http import urlsafe_base64_encode
+from django.utils.translation import gettext_lazy as _
+from django.urls import reverse
 
 from .tasks import send_multi_alternatives_mail
 
@@ -119,4 +121,35 @@ class UMPasswordResetForm(PasswordResetForm):
             self.send_mail(
                 subject_template_name, email_template_name, context, from_email,
                 user.email, html_email_template_name=html_email_template_name,
+            )
+
+
+class UMAuthenticationForm(AuthenticationForm):
+
+    def confirm_login_allowed(self, user):
+        """
+        Controls whether the given User may log in. This is a policy setting,
+        independent of end-user authentication. This default behavior is to
+        allow login by active users, and reject login by inactive users.
+
+        If the given user cannot log in, this method should raise a
+        ``forms.ValidationError``.
+
+        If the given user may log in, this method should return None.
+        """
+        if not user.is_active:
+            raise forms.ValidationError(
+                self.error_messages['inactive'],
+                code='inactive',
+            )
+
+        if not user.email_confirmed:
+            raise forms.ValidationError(
+                # Error message cannot be added to self.error_messages, as usage of reverse() function at a class definition level causes
+                # in this case form import error.
+                _(
+                    f'Please confirm your email before continuation.'
+                    f'Do you want to <a href="{reverse("resend_verification_email")}">re-send activation email</a>?'
+                ),
+                code='inactive',
             )
--- project/apps/user_management/urls.py
+++ project/apps/user_management/urls.py
@@ -3,27 +3,54 @@ from apps.user_management.views.account_page import AccountPageView, AccountEdit
 from django.contrib.auth import views as auth_views
 
 from apps.user_management.views.signup import SignupView
-from apps.user_management.forms import UMPasswordResetForm
+from apps.user_management.forms import UMPasswordResetForm, UMAuthenticationForm
 from apps.user_management.views.email_confirmation import EmailConfirmationView, EmailConfirmationSentView, \
     ResendVerificationEmailView
 
 urlpatterns = [
     # ---- django.contrib.auth.urls.views ----
-    path('accounts/login/', UMLoginView.as_view(), name='login'),
-    path('accounts/logout/', auth_views.LogoutView.as_view(), name='logout'),
-    path('accounts/password_change/',
-         auth_views.PasswordChangeView.as_view(template_name='account/password_change_form.html'),
-         name='password_change'),
-    path('accounts/password_change/done/',
-         auth_views.PasswordChangeDoneView.as_view(template_name='account/password_change_done.html'),
-         name='password_change_done'),
-    path('accounts/password_reset/', auth_views.PasswordResetView.as_view(form_class=UMPasswordResetForm),
-         name='password_reset'),
-    path('accounts/password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
-    path('accounts/reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(),
-         name='password_reset_confirm'),
-    path('accounts/reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+    path(
+        'accounts/login/',
+        UMLoginView.as_view(form_class=UMAuthenticationForm),
+        name='login'
+    ),
+    path(
+        'accounts/logout/',
+        auth_views.LogoutView.as_view(),
+        name='logout'
+    ),
+    path(
+        'accounts/password_change/',
+        auth_views.PasswordChangeView.as_view(template_name='account/password_change_form.html'),
+        name='password_change'
+    ),
+    path(
+        'accounts/password_change/done/',
+        auth_views.PasswordChangeDoneView.as_view(template_name='account/password_change_done.html'),
+        name='password_change_done'
+    ),
+    path(
+        'accounts/password_reset/',
+        auth_views.PasswordResetView.as_view(form_class=UMPasswordResetForm),
+        name='password_reset'
+    ),
+    path(
+        'accounts/password_reset/done/',
+        auth_views.PasswordResetDoneView.as_view(),
+        name='password_reset_done'
+    ),
+    path(
+        'accounts/reset/<uidb64>/<token>/',
+        auth_views.PasswordResetConfirmView.as_view(),
+        name='password_reset_confirm'
+    ),
+    path(
+        'accounts/reset/done/',
+        auth_views.PasswordResetCompleteView.as_view(),
+        name='password_reset_complete'
+    ),
     # ---- end of: django.contrib.auth.urls.views ----
+
     path(r'user/register/', SignupView.as_view(), name='register'),
     re_path(r'user/account/edit/(?P<username>\w+)/', AccountEditView.as_view(), name='user_account_edit'),
 
