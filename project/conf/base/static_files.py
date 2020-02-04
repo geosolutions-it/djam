e9b21e83eb28ba8934a6a2d33a21892a7967c8c5
giohappy <giohappy@gmail.com>
2020-02-04 12:59:30 +0100

    sekelton files and configs for base template

--- project/conf/base/static_files.py
+++ project/conf/base/static_files.py
@@ -5,4 +5,8 @@ from conf.base.base import BASE_DIR
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.getenv('DJAM_STATIC_ROOT',os.path.join(BASE_DIR, 'static_root'))
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
