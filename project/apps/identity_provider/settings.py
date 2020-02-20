d282d13af48400bd5f2a2b48c3068347bcc193e3
bieganowski <bieganowski.rev@gmail.com>
2020-02-20 11:31:02 +0100

    geoserver basic auth introspection

--- project/apps/identity_provider/settings.py
+++ project/apps/identity_provider/settings.py
@@ -10,3 +10,4 @@ IP_ENABLE_OAUTH2_MANAGEMENT_URLPATTERNS = True
 # django-oidc-provider custom scope claims conifg
 OIDC_EXTRA_SCOPE_CLAIMS = 'project.apps.identity_provider.oidc_provider_settings.CustomScopeClaims'
 
+REQUIRE_SECURE_HTTP_FOR_GEOSERVER_INTROSPECTION = True
