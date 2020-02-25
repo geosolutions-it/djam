41f33b189e94999395ddf6b501116f00172c9b2c
bieganowski <57660086+bieganowski@users.noreply.github.com>
2020-02-25 12:08:17 +0100

    path fix (#119)

--- project/apps/identity_provider/settings.py
+++ project/apps/identity_provider/settings.py
@@ -8,6 +8,6 @@ IP_ENABLE_OAUTH2_MANAGEMENT_URLPATTERNS = True
 
 
 # django-oidc-provider custom scope claims conifg
-OIDC_EXTRA_SCOPE_CLAIMS = 'project.apps.identity_provider.oidc_provider_settings.CustomScopeClaims'
+OIDC_EXTRA_SCOPE_CLAIMS = 'apps.identity_provider.oidc_provider_settings.CustomScopeClaims'
 
 REQUIRE_SECURE_HTTP_FOR_GEOSERVER_INTROSPECTION = True
