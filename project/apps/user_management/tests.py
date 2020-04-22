01e3f33ad60f48592e4e72e741a0dff24dabb303
Shane Dowling <shane@shanedowling.com>
2020-04-22 14:34:30 +0100

    chore: more user management tests

--- project/apps/user_management/tests.py
+++ project/apps/user_management/tests.py
@@ -5,23 +5,24 @@ import copy
 from apps.user_management.models import User, UserActivationCode
 
 
-class UserTests(TestCase):
-    def create_user(self, username="test_user", email="johnsmith@example.net", **kwargs):
-        return User.objects.create(username=username, email=email, **kwargs)
+def create_user(username="test_user", email="johnsmith@example.net", **kwargs):
+    return User.objects.create(username=username, email=email, **kwargs)
 
+
+class UserTests(TestCase):
     def test_user_creation(self):
-        u = self.create_user()
+        u = create_user()
         self.assertTrue(isinstance(u, User))
 
     def test_names(self):
         first_name = "John"
         last_name = "Smith"
-        u = self.create_user(first_name=first_name, last_name=last_name)
+        u = create_user(first_name=first_name, last_name=last_name)
         self.assertEquals(u.get_full_name(), first_name + " " + last_name)
         self.assertEquals(u.get_short_name(), first_name)
 
     def test_cleaning(self):
-        u = self.create_user(email="johnsmith@EXAMPLE.net")
+        u = create_user(email="johnsmith@EXAMPLE.net")
         u.clean()
         self.assertEquals(u.email, "johnsmith@example.net")
         self.assertEquals(u.email, u.username)
@@ -30,7 +31,7 @@ class UserTests(TestCase):
 class UserActivationCodeTests(TestCase):
 
     def create_user_activation(self):
-        u = User.objects.create(email="johnsmith@example.net")
+        u = create_user(email="johnsmith@example.net")
         ua = UserActivationCode(user=u)
 
         return ua
@@ -45,3 +46,17 @@ class UserActivationCodeTests(TestCase):
         self.assertNotEquals(ua_original.creation_date, ua_date)
 
 
+class UserPostSaveActivationCode(TestCase):
+    def test_user_activation_post_hook(self):
+        u = create_user(email="johnsmith@example.net")
+        self.assertEquals(
+            1,
+            UserActivationCode.objects.filter(user=u).count()
+        )
+
+class UserPreSaveActivationEmail(TestCase):
+    def test_user_activation_pre_hook(self):
+        # TODO - This pre hook may need 
+        # refactoring and something like DI
+        # so we can mock and test
+        pass
