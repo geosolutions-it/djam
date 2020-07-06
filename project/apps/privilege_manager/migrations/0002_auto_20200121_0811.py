23dd4ef6400d15df6c84fde6380108c7c3fb534e
bieganowski <bieganowski.rev@gmail.com>
2020-07-06 10:28:31 +0200

    adding default admin group

--- project/apps/privilege_manager/migrations/0002_auto_20200121_0811.py
+++ project/apps/privilege_manager/migrations/0002_auto_20200121_0811.py
@@ -19,6 +19,9 @@ def add_default_groups(apps, schema_editor):
     hub_group = Group(name="hub")
     hub_group.save()
 
+    admin_group = Group(name="admin")
+    admin_group.save()
+
 
 class Migration(migrations.Migration):
 
