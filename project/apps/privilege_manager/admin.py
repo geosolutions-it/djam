7013e9294eb4df976ed68af25a1133003743635c
Piotr Dankowski <dankowski.revolve@gmail.com>
2020-07-10 15:15:14 +0200

    add apikey creation/revoke mechanizm when user is added to enterprise group

--- project/apps/privilege_manager/admin.py
+++ project/apps/privilege_manager/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
+
+from apps.identity_provider.models import ApiKey
 from apps.privilege_manager.models import Group, OpenIdLoginPrevention
 from django.contrib.auth.models import Group as DefaultGroup
-
 
 # un-registration of Django auth Group model in admin page
 admin.site.unregister(DefaultGroup)
@@ -13,3 +15,25 @@ class GroupAdmin(admin.ModelAdmin):
     list_display = ("name",)
     ordering = ("name",)
     search_fields = ("name",)
+
+    def save_model(self, request, obj, form, change):
+        if Group.GroupNames.ENTERPRISE.value == obj.name:
+            self._generate_api_key_for_enterprise_user(form)
+
+        super(GroupAdmin, self).save_model(request, obj, form, change)
+
+    def _generate_api_key_for_enterprise_user(self, form):
+        if 'users' in form.changed_data:
+            initial_users = set(form.initial.get('users'))
+            new_users = set(form.cleaned_data.get('users'))
+            users_to_create = new_users - initial_users
+
+            if users_to_create != set():
+                # for this users create api keys
+                for user in users_to_create:
+                    ApiKey.objects.create(user=user)
+
+            users_to_revoke = initial_users - new_users
+            if users_to_revoke != set():
+                # for this users revoke api keys
+                ApiKey.objects.filter(user__in=users_to_revoke).update(revoked=True)
