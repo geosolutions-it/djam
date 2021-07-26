4bbddf14bbccc68c1af830332a98a3957bf00856
mattiagiupponi <mattia.giupponi@gmail.com>
2021-07-26 15:40:47 +0200

    Issue #236: handle account upgrade/downgrade

--- project/apps/administration/admin.py
+++ project/apps/administration/admin.py
@@ -1,8 +1,11 @@
 from django.contrib.auth import get_user_model
+import django.http
+from django.shortcuts import redirect
+from django.urls.conf import path
 from apps.administration.models import AccountManagementModel
-from django.contrib import admin
+from django.contrib import admin, messages
 from django.utils.translation import ugettext_lazy as _
-
+from django.contrib.admin.views.main import ChangeList
 # Register your models here.
 
 @admin.register(AccountManagementModel)
@@ -18,6 +21,14 @@ class AccountManagementAdmin(admin.ModelAdmin):
     ]
 
     search_fields = ['username']
+
+    def get_urls(self):
+        urls = super().get_urls()
+        my_urls = [
+            path('upgrade/', self.account_upgrade, name="account_upgrade"),
+            path('downgrade/', self.account_downgrade, name="account_downgrade")
+        ]
+        return my_urls + urls
 
     def get_queryset(self, request):
         qs = get_user_model().objects.all().order_by('id')
@@ -41,3 +52,11 @@ class AccountManagementAdmin(admin.ModelAdmin):
     def changelist_view(self, request, extra_context=None):
         extra_context = extra_context or {}
         return super(AccountManagementAdmin, self).changelist_view(request, extra_context=extra_context)
+
+    def account_upgrade(self, request):
+        self.message_user(request, "The Selected account has been upgraded", messages.SUCCESS)
+        return redirect("..")
+
+    def account_downgrade(self, request):
+        self.message_user(request, "The Selected account has been downgraded", messages.SUCCESS)
+        return redirect("..")
