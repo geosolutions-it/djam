c9922cdbc00d661138c2bf2499ea28e6de78dea2
giohappy <giohappy@gmail.com>
2021-11-16 11:36:37 +0100

    fix subscriptions update

--- project/apps/administration/forms.py
+++ project/apps/administration/forms.py
@@ -16,11 +16,12 @@ class CompanySubsForm(forms.ModelForm):
         cleaned_data = super().clean()
         users = cleaned_data['company'].users.all()
         cleaned_data['groups'] = Group.objects.get(name=cleaned_data['groups'])
-        try:
-            subscription_manager.can_add_new_subscription_by_user(users, SubscriptionTypeEnum.COMPANY)
-        except Exception as e:
-            # TODO: add the list of the users to be checked
-            raise forms.ValidationError("One of the users of the selected company, already have a COMPANY subscription. Please verify the company settings before create the subscription")
+        if self.instance and not self.instance.pk:
+            try:
+                subscription_manager.can_add_new_subscription_by_user(users, SubscriptionTypeEnum.COMPANY)
+            except Exception as e:
+                # TODO: add the list of the users to be checked
+                raise forms.ValidationError("One of the users of the selected company, already have a COMPANY subscription. Please verify the company settings before create the subscription")
 
         return cleaned_data
 
@@ -30,10 +31,11 @@ class IndividualSubsForm(forms.ModelForm):
     def clean(self):
         cleaned_data = super().clean()
         cleaned_data['groups'] = Group.objects.get(name=cleaned_data['groups'])
-        try:
-            subscription_manager.can_add_new_subscription_by_user(cleaned_data['user'], SubscriptionTypeEnum.INDIVIDUAL)
-        except Exception as e:
-            # TODO: add the list of the users to be checked
-            raise forms.ValidationError("The selected user already have an active INDIVIDUAL subscription.")
+        if self.instance and not self.instance.pk:
+            try:
+                subscription_manager.can_add_new_subscription_by_user(cleaned_data['user'], SubscriptionTypeEnum.INDIVIDUAL)
+            except Exception as e:
+                # TODO: add the list of the users to be checked
+                raise forms.ValidationError("The selected user already have an active INDIVIDUAL subscription.")
 
         return cleaned_data
