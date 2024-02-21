3ebf423d8ab907a52e5b229eb7b7d8abd214242d
mattiagiupponi <mattia.giupponi@gmail.com>
2024-02-21 15:04:09 +0100

    Test Docker Compose deployment

--- project/docker-entrypoint.sh
+++ project/docker-entrypoint.sh
@@ -1,4 +1,7 @@
 python -W ignore manage.py collectstatic -c --noinput
 python -W ignore manage.py migrate --noinput
 python -W ignore manage.py creatersakey
-uwsgi --ini djam.ini & python manage.py rundramatiq
+echo "running dramatiq"
+nohup python manage.py rundramatiq &
+echo "running django"
+python manage.py runserver 0.0.0.0:8000
