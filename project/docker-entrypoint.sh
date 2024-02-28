3e5232bb133a75281d950ec86bd1966e5472c3f2
mattiagiupponi <mattia.giupponi@gmail.com>
2024-02-28 16:09:57 +0100

    Fixes #9 Enable gunicorn to run djam

--- project/docker-entrypoint.sh
+++ project/docker-entrypoint.sh
@@ -4,6 +4,5 @@ python -W ignore manage.py creatersakey
 echo "running dramatiq"
 nohup python manage.py rundramatiq &
 echo "running django"
-python manage.py runserver 0.0.0.0:8000
 cd /djam/project
 DJANGO_SETTINGS_MODULE=settings gunicorn wsgi
