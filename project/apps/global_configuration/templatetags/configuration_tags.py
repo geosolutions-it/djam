20221367cd47e9d5b275381ad23b7d604b4df040
mattiagiupponi <mattia.giupponi@gmail.com>
2024-02-28 12:59:08 +0100

    Fixes #9 Remove map button

--- project/apps/global_configuration/templatetags/configuration_tags.py
+++ project/apps/global_configuration/templatetags/configuration_tags.py
@@ -3,8 +3,3 @@ from django import template
 from apps.global_configuration.models import GlobalConfiguration
 
 register = template.Library()
-
-
-@register.simple_tag
-def get_map_url():
-    return GlobalConfiguration.load().map_redirect_url
