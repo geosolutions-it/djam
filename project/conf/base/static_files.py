4b22af0c7eee69f7a73711fe4b33ef9c888559cd
G. Allegri <giohappy@gmail.com>
2024-05-02 14:03:49 +0200

    improved docker compose for local dev

--- project/conf/base/static_files.py
+++ project/conf/base/static_files.py
@@ -3,10 +3,7 @@ from conf.base.base import BASE_DIR
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-STATICFILES_LOCATION = "static"
-
-STATIC_URL = os.environ.get("STATIC_URL", "")
-STATIC_URL = f"/{STATICFILES_LOCATION}/"
+STATIC_URL = os.environ.get("STATIC_URL", "static/")
 STATIC_ROOT = os.getenv("DJAM_STATIC_ROOT", os.path.join(BASE_DIR, "static_root"))
 
 STATICFILES_DIRS = [
--- project/docker-entrypoint.sh
+++ project/docker-entrypoint.sh
@@ -1,8 +1,14 @@
 python -W ignore manage.py collectstatic --noinput
 python -W ignore manage.py migrate --noinput
+python -W ignore manage.py loaddata example_admin --noinput
 python -W ignore manage.py creatersakey
 echo "running dramatiq"
 nohup python manage.py rundramatiq -p 1 -t 1  &
 echo "running django"
 cd /djam/project
-DJANGO_SETTINGS_MODULE=settings gunicorn wsgi --bind=0.0.0.0:8000
+
+if [ "$DJAM_PROJECT_ENVIRONMENT" == "dev" ]; then
+    python manage.py runserver 0.0.0.0:8000
+else
+  DJANGO_SETTINGS_MODULE=settings gunicorn wsgi --bind=0.0.0.0:8000
+fi
