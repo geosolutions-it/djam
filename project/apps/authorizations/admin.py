ea4ed35e4dfbd29c391b19c5bbb9c0db48be5b0b
mattiagiupponi <mattia.giupponi@gmail.com>
2024-05-09 13:04:28 +0200

    Format with black

--- project/apps/authorizations/admin.py
+++ project/apps/authorizations/admin.py
@@ -12,38 +12,47 @@ from apps.privilege_manager.models import Team
 admin.site.register(AccessRule)
 admin.site.register(Resource)
 
+
 class RoleForm(forms.ModelForm):
     name = forms.CharField(max_length=200)
     user = forms.ModelMultipleChoiceField(
-        queryset=get_user_model().objects, 
+        queryset=get_user_model().objects,
         required=False,
-        widget=FilteredSelectMultiple(verbose_name="users", is_stacked=False)
+        widget=FilteredSelectMultiple(verbose_name="users", is_stacked=False),
     )
     team = forms.ModelMultipleChoiceField(
-        queryset=Team.objects, 
+        queryset=Team.objects,
         required=False,
-        widget=FilteredSelectMultiple(verbose_name="users", is_stacked=False)
+        widget=FilteredSelectMultiple(verbose_name="users", is_stacked=False),
     )
+
     class Meta:
         model = Role
-        fields = ("name", "user",)
+        fields = (
+            "name",
+            "user",
+        )
 
     def __init__(self, *args, **kwargs):
         super(RoleForm, self).__init__(*args, **kwargs)
         if self.instance:
             is_insert = self.instance.pk is None
-            self.fields["user"].initial = [] if is_insert else self.instance.user_set.all() 
-            self.fields["team"].initial = [] if is_insert else self.instance.team_set.all() 
+            self.fields["user"].initial = (
+                [] if is_insert else self.instance.user_set.all()
+            )
+            self.fields["team"].initial = (
+                [] if is_insert else self.instance.team_set.all()
+            )
 
     def save(self, commit: bool = ...) -> Any:
-        self.instance.user_set.add(*[u.id for u in self.cleaned_data['user']])
-        self.instance.team_set.add(*[u.id for u in self.cleaned_data['team']])
+        self.instance.user_set.add(*[u.id for u in self.cleaned_data["user"]])
+        self.instance.team_set.add(*[u.id for u in self.cleaned_data["team"]])
         return super().save(commit=commit)
 
 
 @admin.register(Role)
 class RoleAdmin(admin.ModelAdmin):
     form = RoleForm
-    
+
     def __str__(self) -> str:
         return self.name
--- project/apps/authorizations/models.py
+++ project/apps/authorizations/models.py
@@ -2,13 +2,14 @@ from django.db import models
 
 # Create your models here.
 
+
 class Resource(models.Model):
-    
+
     """
     A generic Resource model, that will be used by other components to implement rules-based access to their own resources and services
     A Resource will contain a generic "path" attribute, whose meaning is only known to the services (e.g. the Gateway app) that will use the rule.
     """
-    
+
     name = models.CharField(verbose_name="Name")
     path = models.CharField(verbose_name="path")
 
