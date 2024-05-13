e6fec4a484f6fc4fb3a17048d5836ebf04aba9cc
Mattia <mattia.giupponi@gmail.com>
2024-05-13 14:55:45 +0200

    [Fixes #28] Let the user authenticate via basic auth or djam authkey

--- project/apps/identity_provider/views/geoserver_integration.py
+++ project/apps/identity_provider/views/geoserver_integration.py
@@ -120,7 +120,7 @@ class GeoserverAuthKeyAndApiKeyIntrospection(GeoserverIntrospection, views.APIVi
             )
 
         user = get_user_model().objects.get(id=user_id)
-        user_groups_names = [user.get_team()]
+        user_groups_names = user.get_team()
 
         return Response({"username": user.username, "groups": user_groups_names,})
 
@@ -133,7 +133,7 @@ class GeoserverAuthKeyAndApiKeyIntrospection(GeoserverIntrospection, views.APIVi
             raise ValidationError(f"API key {api_key.key} is revoked.")
 
         user = api_key.user
-        user_groups_names = [user.get_team()]
+        user_groups_names = user.get_team()
 
         if str(api_key.wms_key) == api_key_uuid:
             # add "_wms" suffix to the group names
